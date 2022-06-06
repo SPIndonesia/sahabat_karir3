2f0e72a96de0a6f03af691c1c6f2406afa879f17
ADDFa <fratamaadha18@gmail.com>
2022-06-06 17:19:37 +0700

    Perbaikan header template, Desktop Mode

--- resources/views/templates/templateSK.blade.php
+++ resources/views/templates/templateSK.blade.php
@@ -7,8 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SAHABAT KARIR</title>
 
-    <script type="text/javascript" src="https://app.midtrans.com/snap/snap.js" data-client-key="<Mid-client-j6xwra16OXDlv3sl>"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/02db274a60.js" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="https://app.midtrans.com/snap/snap.js"
+        data-client-key="<Mid-client-j6xwra16OXDlv3sl>"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <!-- --------------- icon website --------------- -->
     <link rel="shortcut icon" href="{{asset('assets/img/icon/logo-sk.png')}}">
 
@@ -21,34 +25,32 @@
 
 <body>
     <header>
-        <div class="sahabat-karir-logo">
-            <img src="{{asset('assets/img/icon/logo-sk.png')}}" alt="sahabat karir">
+        <div class="header-left">
+            <div class="logo">
+                <img src="{{asset('assets/img/icon/logo-sk.png')}}" alt="sahabat karir">
+            </div>
+
+            <nav class="nav-header" id="nav">
+                <a href="kelas">Kelas</a>
+                <a href="tentang-kami">Tentang Kami</a>
+                <a href="pelanggan">Pelanggan</a>
+            </nav>
         </div>
 
-        <nav class="navigasi-halaman-landing" id="nav">
-            <a href="kelas">Kelas</a>
-            <a href="tentang-kami">Tentang Kami</a>
-            <a href="pelanggan">Pelanggan</a>
-        </nav>
+        <div class="header-right">
+            <div class="tombol">
+                <a href="">Login</a>
+                <a href="">Register</a>
 
-        <form class="pencarian" action="" method="POST">
-            <input type="text" placeholder="Searching">
+                <a href="" hidden class="logout">Logout</a>
+                <a href="" hidden class="admin">Admin</a>
+            </div>
 
-            <svg class="icon-cari" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z">
-            </svg>
-
-            <button class="header__cari" type="submit" hidden></button>
-        </form>
-
-        <a href="keranjangBelanja.html" class="keranjang-belanja">
-            <i class="fa-solid fa-cart-shopping"></i>
-        </a>
-
-        <div class="hamburger-navigasi">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <path d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z" />
-            </svg>
+            <div class="keranjang">
+                <a href="">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </a>
+            </div>
         </div>
     </header>
 
@@ -114,12 +116,14 @@
 
                 <ul>
                     <li class="email">
-                        <a target="_blank" href="mailto:sahabatprofesionalindonesia@gmail.com">sahabatprofesionalindonesia@gmail.com</a>
+                        <a target="_blank"
+                            href="mailto:sahabatprofesionalindonesia@gmail.com">sahabatprofesionalindonesia@gmail.com</a>
                     </li>
 
                     <li class="lokasi">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                            <path d="M168.3 499.2C116.1 435 0 279.4 0 192C0 85.96 85.96 0 192 0C298 0 384 85.96 384 192C384 279.4 267 435 215.7 499.2C203.4 514.5 180.6 514.5 168.3 499.2H168.3zM192 256C227.3 256 256 227.3 256 192C256 156.7 227.3 128 192 128C156.7 128 128 156.7 128 192C128 227.3 156.7 256 192 256z" />
+                            <path
+                                d="M168.3 499.2C116.1 435 0 279.4 0 192C0 85.96 85.96 0 192 0C298 0 384 85.96 384 192C384 279.4 267 435 215.7 499.2C203.4 514.5 180.6 514.5 168.3 499.2H168.3zM192 256C227.3 256 256 227.3 256 192C256 156.7 227.3 128 192 128C156.7 128 128 156.7 128 192C128 227.3 156.7 256 192 256z" />
                         </svg>
                         <p>
                             Jalan Kerapu Nomor 44 Kelurahan Berkas Kecamatan Teluk Segara Kota Bengkulu
@@ -135,7 +139,6 @@
     <script src="{{asset('assets/js/slick.min.js')}}"></script>
 
     <!-- --------------- js --------------- -->
-    <script src="https://kit.fontawesome.com/02db274a60.js')}}" crossorigin="anonymous"></script>
     <script src="{{asset('assets/js/landingPage.js')}}"></script>
 </body>
 
