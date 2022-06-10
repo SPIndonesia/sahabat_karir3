f0e12c565fcab30be8c1786f9d50c381538a896d
Rwhytm <rajuwahyudi842@gmail.com>
2022-06-10 11:03:09 +0700

    add seeder for kategori and paket table

---
+++ database/seeders/KategoriSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kategori;
+use Illuminate\Database\Seeder;
+
+class KategoriSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Kategori::create([
+            [
+                'nama' => 'SBMPTN',
+                'deskripsi' => 'ini deskripsi',
+                'image_url' => 'foto.png',
+            ],
+            [
+                'nama' => 'Polri',
+                'deskripsi' => 'ini deskripsi',
+                'image_url' => 'foto.png',
+            ]
+        ]);
+    }
+}
---
+++ database/seeders/PaketSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Paket;
+use Illuminate\Database\Seeder;
+
+class PaketSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Paket::create([
+            [
+                'nama' => 'paket1',
+                'harga' => 1000000,
+                'deskripsi' => 'ini deskripsi',
+                'image_url' => 'foto.png',
+                'id_kategori' => 1,
+            ],
+            [
+                'nama' => 'paket2',
+                'harga' => 2000000,
+                'deskripsi' => 'ini deskripsi',
+                'image_url' => 'foto.png',
+                'id_kategori' => 1
+            ]
+
+        ]);
+    }
+}
